f76e78838f49b38dd9626b01d20a96351a57d9d8
pannal <panni@fragstore.net>
2024-07-12 18:10:50 +0200

    Templating: Add math operation filters/builtins (in case anyone needs them)

--- lib/templating/filters.py
+++ lib/templating/filters.py
@@ -2,6 +2,8 @@
 
 import ibis
 import copy
+import operator
+import six
 
 from .util import register_builtin
 
@@ -10,6 +12,42 @@ from .util import register_builtin
 def get_attr(obj, attr):
     return obj.get(attr)
 
+
+@ibis.filters.register('calc')
+@register_builtin
+def calc(a, b, op="add"):
+    if isinstance(a, six.string_types):
+        a = float(a) if "." in a else int(a)
+    elif isinstance(b, six.string_types):
+        b = float(b) if "." in b else int(b)
+    try:
+        return getattr(operator, op)(a, b)
+    except:
+        raise ValueError("Can't calculate {}({}:{}, {}:{})".format(op, type(a), repr(a), type(b), repr(b)))
+
+
+@ibis.filters.register('add')
+@register_builtin
+def add(a, b):
+    return calc(a, b)
+
+
+@ibis.filters.register('sub')
+@register_builtin
+def sub(a, b):
+    return calc(a, b, op="sub")
+
+
+@ibis.filters.register('div')
+@register_builtin
+def div(a, b):
+    return calc(a, b, op="truediv")
+
+
+@ibis.filters.register('mul')
+@register_builtin
+def mul(a, b):
+    return calc(a, b, op="mul")
 
 @ibis.filters.register('resolve')
 @register_builtin('resolve')
